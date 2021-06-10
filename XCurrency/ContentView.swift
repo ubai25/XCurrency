8d50585b7d4494984a90cb239e71bdf57eaa4aae
Ubaidillah Ahmad <a.ubai25@gmail.com>
2021-06-10 22:46:30 +0700

    Initial Commit

---
+++ XCurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCurrency
+//
+//  Created by Ahmad Ubaidillah on 10/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCurrency/XCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCurrencyApp.swift
+//  XCurrency
+//
+//  Created by Ahmad Ubaidillah on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct XCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
